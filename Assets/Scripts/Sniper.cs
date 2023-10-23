db8012357f4074e825e85b1aebdde44ec33468df
nvellatu <naveen11304@gmail.com>
2023-10-23 16:45:12 -0400

    changed getDamage functions in tank and sniper

--- Assets/Scripts/Sniper.cs
+++ Assets/Scripts/Sniper.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Sniper : IPiece
 {
     public static int movement = 1;
+    // public int GetDamage()
+    // {
+    //     Random random = new Random();
+    //     int randomNumber = random.Next(1, 101);
+    //     return (randomNumber <= 30) ? 1 :
+    //         (randomNumber <= 60) ? 2 :
+    //         (randomNumber <= 85) ? 3 :
+    //         4;
+    // }
     public int GetDamage()
     {
-        Random random = new Random();
-        int randomNumber = random.Next(1, 101);
+        int randomNumber = UnityEngine.Random.Range(1, 101);
         return (randomNumber <= 30) ? 1 :
             (randomNumber <= 60) ? 2 :
             (randomNumber <= 85) ? 3 :
--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class Tank : IPiece
 {
     public static int movement = 1;
+    // public int GetDamage()
+    // {
+    //     Random random = new Random();
+    //     int randomNumber = random.Next(1, 101);
+    //     return (randomNumber <= 50) ? 3 : 4;
+    // }
     public int GetDamage()
     {
-        Random random = new Random();
-        int randomNumber = random.Next(1, 101);
+        int randomNumber = UnityEngine.Random.Range(1, 101);
         return (randomNumber <= 50) ? 3 : 4;
     }
     public static int health = 5;
