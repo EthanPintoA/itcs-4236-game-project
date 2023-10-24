2f51912aa2c122b6f5389cc61b34e44cffcffa57
nvellatu <naveen11304@gmail.com>
2023-10-24 15:14:55 -0400

    fixed king script

---
+++ Assets/Scripts/King.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class King : IPiece
+{
+    public static int movement = 1;
+    public int getDamage()
+    {
+        return 1;
+    }
+    public static int health = 1;
+    public static int range = 1;
+    public GameObject GameObject { get; }
+
+    public Soldier(GameObject gameObject)
+    {
+        GameObject = gameObject;
+    }
+}
