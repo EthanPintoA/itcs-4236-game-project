c3315484e751e7952e73f7d24b6cbba13086d62e
nvellatu <naveen11304@gmail.com>
2023-12-10 21:44:54 -0500

    readded random damage
    won't know if it works 100% until we add in stat viewer

--- Assets/Scripts/Sniper.cs
+++ Assets/Scripts/Sniper.cs
@@ -4,13 +4,11 @@ public class Sniper : IPiece
 {
     public int GetDamage()
     {
-        /*int randomNumber = Random.Range(1, 101);
+        int randomNumber = Random.Range(1, 101);
         return (randomNumber <= 30) ? 1 :
             (randomNumber <= 60) ? 2 :
             (randomNumber <= 85) ? 3 :
             4;
-        */
-        return 1;
     }
 
     public GameObject GameObject { get; }
